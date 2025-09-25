6628352c14b2c158b2be856129149090b114efe5
maithili28 <mmaithili15@gmail.com>
2025-09-26 02:18:44 +0530

    Update default server port to 8080

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ import app from "./app.js";
 //   .on("error", (err) => {
 //     console.error("Server failed to start:", err);
 //   });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
