2ab5a2a5c49f601c1a08d62b13aac565bcbe61ac
maithili28 <mmaithili15@gmail.com>
2025-09-26 03:01:13 +0530

    Update static file serving for React frontend and simplify route handling

--- app.js
+++ app.js
@@ -37,10 +37,15 @@ app.use("/api", portfolioRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../Personal-Portfolio/dist")));
+// app.use(express.static(path.join(__dirname, "../Personal-Portfolio/dist")));
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../Personal-Portfolio/dist/index.html"));
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, "../Personal-Portfolio/dist/index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 export default app;
